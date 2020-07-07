2acc5a6dc836ba4eb85f774032e963234bcdbcb6
Cristian Deleon <libaration@gmail.com>
2020-07-07 02:15:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  myArray = ['dog','cat','wolf']
+  myArray = ['dog','cat','wolf','ape']
 end
 
 def add_element_to_end_of_array(array, element)
